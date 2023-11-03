eeb4aa85ae68fcbeab87e8715b30418b0df5e484
pahlavidavlatmirov <p.davlatmirov99@gmail.com>
2023-11-03 22:57:43 +0600

    6th commit(zebra color was added)

--- app/src/main/java/com/example/practice1bottomnav/ui/home/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/practice1bottomnav/ui/home/adapter/TaskAdapter.kt
@@ -1,18 +1,26 @@
 package com.example.practice1bottomnav.ui.home.adapter
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.practice1bottomnav.R
 import com.example.practice1bottomnav.databinding.ItemTaskBinding
-import com.example.practice1bottomnav.ui.model.Task
+import com.example.practice1bottomnav.model.Task
+import kotlinx.coroutines.joinAll
+import kotlin.reflect.KFunction1
 
 class TaskAdapter(
-    private val onLongClick: (Task) ->Unit
+    private val onLongClick: (Task) ->Unit,
+    private val onClick: KFunction1<Task, Unit>
 ) : Adapter<TaskAdapter.TaskViewHolder>() {
 
     private val list = arrayListOf<Task>()
-
     fun addTasks(tasks: List<Task>){
         list.clear()
         list.addAll(tasks)
@@ -27,7 +35,7 @@ class TaskAdapter(
             )
         )
     }
-    override fun onBindViewHolder(holder: TaskAdapter.TaskViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         holder.bind(list[position])
     }
     override fun getItemCount(): Int {
@@ -36,11 +44,18 @@ class TaskAdapter(
     inner class TaskViewHolder(private val binding: ItemTaskBinding) : ViewHolder(binding.root)
     {
         fun bind(task: Task){
+            itemView.setBackgroundColor(if (adapterPosition % 2 == 0) Color.BLACK else Color.WHITE)
+            binding.tvTitle.setTextColor(if (adapterPosition % 2 == 0) Color.WHITE else Color.BLACK)
+            binding.tvDesc.setTextColor(if (adapterPosition % 2 == 0) Color.WHITE else Color.BLACK)
+
             binding.tvTitle.text = task.title
             binding.tvDesc.text = task.desc
             binding.root.setOnLongClickListener {
                 onLongClick(task)
                 true
+            }
+            binding.root.setOnClickListener {
+                onClick(task)
             }
         }
     }
